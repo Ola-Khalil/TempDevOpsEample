3e88ee54d45d835c43c686eab8b62b78e8a340c5
your_usernameOla <your_email_address@example.com>
2023-12-17 23:16:18 +0200

    Scaffold models from existing database in azure

---
+++ src/webapi/Models/Person.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace webapi.Models;
+
+public partial class Person
+{
+    public int PersonId { get; set; }
+
+    public string LastName { get; set; } = null!;
+
+    public string? FirstName { get; set; }
+
+    public int? Age { get; set; }
+}
