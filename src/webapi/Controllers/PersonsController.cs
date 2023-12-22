b1ab61bff8908eff1c5d6867a0aedd643632b3c9
your_usernameOla <your_email_address@example.com>
2023-12-22 12:53:39 +0200

    PersonController

--- src/webapi/Controllers/PersonsController.cs
+++ src/webapi/Controllers/PersonsController.cs
@@ -1,54 +1,41 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
-using webapi.Models;
+using WebAPI.Models;
 
-namespace webapi.Controllers
+namespace WebAPI.Controllers
 {
-    [ApiController]
     [Route("api/[controller]")]
-
-    public class PersonsController : Controller
+    [ApiController]
+    public class PersonController : ControllerBase
     {
         private readonly KoodaajakoulutusContext _context;
 
-        public PersonsController(KoodaajakoulutusContext context)
+        public PersonController(KoodaajakoulutusContext context)
         {
             _context = context;
         }
 
-        // GET: Persons
-        [HttpGet(Name = "GetPersons")]
-        
-        public IEnumerable<Person> Indexxx()
+        // GET: api/Person
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Person>>> GetPersons()
         {
-            //Fetch all persons from database
-            Person[] allPeople = _context.Persons.ToArray();
- 
-
-            return allPeople;
-
-
-            // return _context.Persons != null ?
-           //  View(await _context.Persons.ToListAsync()):
-           //  Problem("Entity set 'KoodaajakoulutusContext.Persons'  is null.");
+          if (_context.Persons == null)
+          {
+              return NotFound();
+          }
+            return await _context.Persons.Skip(1).ToListAsync();
         }
 
-        // GET: Persons/Details/5
-        public async Task<IActionResult> GetPersons(int id)
+        // GET: api/Person/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Person>> GetPerson(int id)
         {
-            if (_context.Persons == null)
-              {
-                return NotFound();
-              }
-            
+          if (_context.Persons == null)
+          {
+              return NotFound();
+          }
+            var person = await _context.Persons.FindAsync(id);
 
-            var person = await _context.Persons
-                .FirstOrDefaultAsync(id);
             if (person == null)
             {
                 return NotFound();
@@ -57,12 +44,7 @@ namespace webapi.Controllers
             return person;
         }
 
-        // GET: Persons/Create
-        public IActionResult Create()
-        {
-            return View();
-        }
-         // PUT: api/Person/5
+        // PUT: api/Person/5
         // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
         [HttpPut("{id}")]
         public async Task<IActionResult> PutPerson(int id, Person person)
@@ -93,109 +75,44 @@ namespace webapi.Controllers
             return NoContent();
         }
 
-        // POST: Persons/Create
-        // To protect from overposting attacks, enable the specific properties you want to bind to.
-        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        // POST: api/Person
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
         [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([Bind("PersonId,LastName,FirstName,Age")] Person person)
+        public async Task<ActionResult<Person>> PostPerson(Person person)
         {
-            if (ModelState.IsValid)
-            {
-                _context.Add(person);
-                await _context.SaveChangesAsync();
-                return RedirectToAction(nameof(Index));
-            }
-            return View(person);
-        }
-
-        // GET: Persons/Edit/5
-        public async Task<IActionResult> Edit(int? id)
-        {
-            if (id == null)
-            {
-                return NotFound();
-            }
-
-            var person = await _context.Persons.FindAsync(id);
-            if (person == null)
-            {
-                return NotFound();
-            }
-            return View(person);
-        }
-
-        // POST: Persons/Edit/5
-        // To protect from overposting attacks, enable the specific properties you want to bind to.
-        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(int id, [Bind("PersonId,LastName,FirstName,Age")] Person person)
-        {
-            if (id != person.PersonId)
-            {
-                return NotFound();
-            }
-
-            if (ModelState.IsValid)
-            {
-                try
-                {
-                    _context.Update(person);
-                    await _context.SaveChangesAsync();
-                }
-                catch (DbUpdateConcurrencyException)
-                {
-                    if (!PersonExists(person.PersonId))
-                    {
-                        return NotFound();
-                    }
-                    else
-                    {
-                        throw;
-                    }
-                }
-                return RedirectToAction(nameof(Index));
-            }
-            return View(person);
-        }
-
-        // GET: Persons/Delete/5
-        public async Task<IActionResult> Delete(int? id)
-        {
-            if (id == null)
-            {
-                return NotFound();
-            }
-
-            var person = await _context.Persons
-                .FirstOrDefaultAsync(m => m.PersonId == id);
-            if (person == null)
-            {
-                return NotFound();
-            }
-
-            return View(person);
-        }
-
-        // POST: Persons/Delete/5
-        [HttpPost, ActionName("Delete")]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> DeleteConfirmed(int id)
-        {
-            var person = await _context.Persons.FindAsync(id);
-            if (person != null)
-            {
-                _context.Persons.Remove(person);
-            }
-
+          if (_context.Persons == null)
+          {
+              return Problem("Entity set 'FreeAzureSqlContext.Persons'  is null.");
+          }
+            _context.Persons.Add(person);
             await _context.SaveChangesAsync();
-            return RedirectToAction(nameof(Index));
+
+            return CreatedAtAction("GetPerson", new { id = person.Personid }, person);
+        }
+
+        // DELETE: api/Person/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeletePerson(int id)
+        {
+            if (_context.Persons == null)
+            {
+                return NotFound();
+            }
+            var person = await _context.Persons.FindAsync(id);
+            if (person == null)
+            {
+                return NotFound();
+            }
+
+            _context.Persons.Remove(person);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
         }
 
         private bool PersonExists(int id)
         {
-            return _context.Persons.Any(e => e.PersonId == id);
+            return (_context.Persons?.Any(e => e.Personid == id)).GetValueOrDefault();
         }
     }
 }
